9ffa23dc000af3a8b91655d2a1dfc9ee21b1bf29
iMac <mino727@c-able.ne.jp>
2019-06-17 17:30:42 +0900

    Initial Commit

---
+++ UserDict/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UserDict
+//
+//  Created by 松崎勝治 on 2019/06/17.
+//  Copyright © 2019 松崎勝治. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
